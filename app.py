de538512353aedc9524924a8b4e4397afdac8f5f
Omar Crosby <omar.crosby@gmail.com>
2022-07-02 20:41:31 -0400

    Adding health endpoints.

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_healthz import healthz
 from apis import blueprint as api
+
 
 app = Flask(__name__)
 app.register_blueprint(api)
+app.register_blueprint(healthz, url_prefix="/healthz")
 CORS(app)
 
 if __name__ == '__main__':
