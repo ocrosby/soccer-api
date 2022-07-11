3bb791b8b5ba1c89f3b673931f6f1420861deee0
Omar Crosby <omar.crosby@gmail.com>
2022-07-11 14:42:21 -0400

    fix(clubs): Caching the club list

--- api/club.py
+++ api/club.py
@@ -55,26 +55,37 @@ def get_league(club_name, ecnl_clubs, ga_clubs):
 clubs_parser = ns.parser()
 clubs_parser.add_argument("clubs[]", type=str, location="form", action="append")
 
+ecnl_clubs = []
+ga_clubs = []
+
 @ns.route("/league/lookup")
 @ns.expect(clubs_parser)
-class CollegeOrganizations(Resource):
+class LeagueLookup(Resource):
     @ns.doc("league_lookup")
     @ns.response(HTTPStatus.OK.value, "Get the organization list", [league_lookup_model])
     @ns.response(HTTPStatus.BAD_REQUEST.value, "Item not found")
     @ns.marshal_list_with(league_lookup_model)
     def post(self):
         """Lookup club leagues"""
+        global ecnl_clubs
+        global ga_clubs
+
         result = []
 
         search = utils.ClubSearch()
 
         args = clubs_parser.parse_args()
         clubs = args["clubs[]"]
-        ecnl_clubs = search.get_ecnl_clubs()
-        ga_clubs = search.get_ga_clubs()
+
+        if ecnl_clubs is None or len(ecnl_clubs) == 0:
+            ecnl_clubs = search.get_ecnl_clubs()
+
+        if ga_clubs is None or len(ga_clubs) == 0:
+            ga_clubs = search.get_ga_clubs()
 
         for club_name in clubs:
-            club = { "club": club_name, "league": get_league(club_name, ecnl_clubs, ga_clubs) }
+            league = get_league(club_name, ecnl_clubs, ga_clubs)
+            club = { "club": club_name, "league": league }
             result.append(club)
 
         return result
