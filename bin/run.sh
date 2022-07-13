41c443a56f5d2b8f8d99a2c0fa90a2761ef2a746
Omar Crosby <omar.crosby@gmail.com>
2022-07-13 04:42:53 -0400

    feat(gunicorn): Increasing workers to 10

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gunicorn wsgi:app --bind 0.0.0.0:8080 --log-level=info --workers=4 --timeout 600
+gunicorn wsgi:app --bind 0.0.0.0:8080 --log-level=info --workers=10 --timeout 600
